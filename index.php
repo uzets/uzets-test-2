7ffb975591443cc25ea51c36dca25000e34a34bc
uzets <uzman.xtm@gmail.com>
2020-09-23 15:15:52 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea reiciendis eveniet ex exercitationem tempore, similique distinctio ullam nostrum qui? Ab accusantium recusandae vel veritatis impedit. Sequi, fugiat id? Fugit, consectetur!</p>
 </body>
 </html>
